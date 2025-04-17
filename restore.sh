fb1fac0aae0f44f4bb60d5ce82d70ed09e29fe63
Lingsheng Dong <lingsheng_dong@hms.harvard.edu>
2025-04-17 14:43:32 -0400

    clean up

--- restore.sh
+++ restore.sh
@@ -30,6 +30,20 @@ if [ ! -f "$cmdFile" ]; then
     exit 1
 fi
 
+
+echo -e "Please give the number of rows you want to check: "
+read -p "" x </dev/tty
+
+[[ "$x" =~ ^[0-9]+$ ]] || { echo -e "Error: should be a number"; exit 1; }
+         
+head -n "$x" "$cmdFile" | sed 's/\\"/"/g'   
+
+echo -e "Continue to restore .dat files? (y/n): "
+
+read -p "" confirm </dev/tty
+
+[[ "$confirm" == y ]] || { echo -e "Aborted"; exit 0; } 
+
 cat "$cmdFile" | xargs -I {} -P 4 sh -c "{}"
 
 echo All done
